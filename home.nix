f954589109241fa0fa081f6c0229fc9ea8533956
Dom H <dom@hxy.io>
2022-11-06 10:37:54 +0000

    Configure FZF colourscheme for light terminal

--- home.nix
+++ home.nix
@@ -28,7 +28,7 @@
   home.sessionVariables = {
     DIRENV_LOG_FORMAT = ""; # https://github.com/direnv/direnv/issues/68
     FZF_DEFAULT_COMMAND = "rg --files --hidden --glob !.git/";
-    FZF_DEFAULT_OPTS = "--color=dark";
+    FZF_DEFAULT_OPTS = "--color=light";
     MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
   };
 
