54d8e21ed7ad8e238f1b59b8cd7b961abfcd55de
Dom H <dom@hxy.io>
2022-11-11 15:36:45 +0000

    hs: Use bundleIDs instead of app names

--- dotfiles/hammerspoon.lua
+++ dotfiles/hammerspoon.lua
@@ -6,27 +6,28 @@ function toggleApp(myApp)
   if app ~= nil and app:isFrontmost() then
     app:hide()
   else
-    hs.application.launchOrFocus(myApp)
+    hs.application.open(myApp)
   end
 end
 
 myHotkeys = {
-  [","] = "System Preferences",
-  ["c"] = "Google Chrome",
-  ["d"] = "Calendar",
-  ["e"] = "Mail",
-  ["f"] = "Finder",
-  ["h"] = "Hammerspoon",
-  ["i"] = "iA Writer",
-  ["k"] = "kitty",
-  ["l"] = "Slack",
-  ["m"] = "Messages",
-  ["p"] = "Preview",
-  ["s"] = "Safari",
-  ["t"] = "kitty", -- t for terminal, for muscle memory
-  ["u"] = "Music",
+	-- osascript -e 'id of app "Foo"'
+  [","] = "com.apple.systempreferences",
+  ["c"] = "com.google.Chrome",
+  ["d"] = "com.apple.iCal",
+  ["e"] = "com.apple.mail",
+  ["f"] = "com.apple.finder",
+  ["h"] = "org.hammerspoon.Hammerspoon",
+  ["i"] = "pro.writer.mac",
+  ["k"] = "net.kovidgoyal.kitty",
+  ["l"] = "com.tinyspeck.slackmacgap",
+  ["m"] = "com.apple.MobileSMS",
+  ["p"] = "com.apple.Preview",
+  ["s"] = "com.apple.Safari",
+  ["t"] = "net.kovidgoyal.kitty", -- t for terminal, for muscle memory
+  ["u"] = "com.apple.Music",
   ["x"] = "Passwords",
-  ["z"] = "zoom.us",
+  ["z"] = "us.zoom.xos",
 }
 
 for k, v in pairs(myHotkeys) do
