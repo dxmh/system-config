d55cf9a6f74d633366319b22678cd1738c738ab7
Dom H <dom@hxy.io>
2022-11-12 17:42:54 +0000

    hs: Assign common window arrangements to hotkeys

--- dotfiles/hammerspoon.lua
+++ dotfiles/hammerspoon.lua
@@ -1,6 +1,44 @@
 require "scratchpad"
 
-function toggleApp(myApp)
+hs.window.animationDuration = 0
+hs.grid.setMargins({25,25})
+hs.grid.setGrid('10x10')
+
+local hyper = {"control", "option", "command"}
+
+local screenPositions = {
+	left = {x=0, y=0, w=6, h=10},
+	left_top = {x=0, y=0, w=6, h=5},
+	left_bottom = {x=0, y=5, w=6, h=5},
+	right = {x=6, y=0, w=4, h=10},
+	right_top = {x=6, y=0, w=4, h=5},
+	right_bottom = {x=6, y=5, w=4, h=5},
+	big = {x=0, y=0, w=10, h=10},
+	middle = {x=2, y=1, w=6, h=8}
+}
+
+local appKeys = {
+	-- osascript -e 'id of app "Foo"'
+  [","] = "com.apple.systempreferences",
+  ["c"] = "com.google.Chrome",
+  ["d"] = "com.apple.iCal",
+  ["e"] = "com.apple.mail",
+  ["f"] = "com.apple.finder",
+  ["i"] = "pro.writer.mac",
+  ["k"] = "net.kovidgoyal.kitty",
+  ["l"] = "com.tinyspeck.slackmacgap",
+  ["m"] = "com.apple.MobileSMS",
+  ["n"] = "com.apple.Notes",
+  ["p"] = "com.apple.Preview",
+  ["r"] = "com.apple.reminders",
+  ["s"] = "com.apple.Safari",
+  ["t"] = "net.kovidgoyal.kitty", -- t for terminal, for muscle memory
+  ["u"] = "com.apple.Music",
+  ["x"] = "Passwords",
+  ["z"] = "us.zoom.xos",
+}
+
+local function toggleApp(myApp)
   hs.application.enableSpotlightForNameSearches(true)
   local app = hs.application.find(myApp)
   if app ~= nil and app:isFrontmost() then
@@ -10,26 +48,27 @@ function toggleApp(myApp)
   end
 end
 
-myHotkeys = {
-	-- osascript -e 'id of app "Foo"'
-  [","] = "com.apple.systempreferences",
-  ["c"] = "com.google.Chrome",
-  ["d"] = "com.apple.iCal",
-  ["e"] = "com.apple.mail",
-  ["f"] = "com.apple.finder",
-  ["h"] = "org.hammerspoon.Hammerspoon",
-  ["i"] = "pro.writer.mac",
-  ["k"] = "net.kovidgoyal.kitty",
-  ["l"] = "com.tinyspeck.slackmacgap",
-  ["m"] = "com.apple.MobileSMS",
-  ["p"] = "com.apple.Preview",
-  ["s"] = "com.apple.Safari",
-  ["t"] = "net.kovidgoyal.kitty", -- t for terminal, for muscle memory
-  ["u"] = "com.apple.Music",
-  ["x"] = "Passwords",
-  ["z"] = "us.zoom.xos",
-}
-
-for k, v in pairs(myHotkeys) do
-  hs.hotkey.bind({"control", "option", "command"}, k, function() toggleApp(v) end)
+for k, v in pairs(appKeys) do
+  hs.hotkey.bind(hyper, k, function() toggleApp(v) end)
 end
+
+hs.hotkey.bind(hyper, "1", function()
+	hs.grid.set(hs.application.open("com.google.Chrome"):mainWindow(), screenPositions.left)
+	hs.grid.set(hs.application.open("net.kovidgoyal.kitty"):mainWindow(), screenPositions.right)
+end)
+
+hs.hotkey.bind(hyper, "2", function()
+	hs.grid.set(hs.application.open("us.zoom.xos"):mainWindow(), screenPositions.left_top)
+	hs.grid.set(hs.application.open("pro.writer.mac"):mainWindow(), screenPositions.left_bottom)
+	hs.grid.set(hs.application.open("com.google.Chrome"):mainWindow(), screenPositions.right)
+end)
+
+hs.hotkey.bind(hyper, "3", function()
+	hs.grid.set(hs.application.open("com.apple.iCal"):mainWindow(), screenPositions.left)
+	hs.grid.set(hs.application.open("com.apple.reminders"):mainWindow(), screenPositions.right)
+end)
+
+hs.hotkey.bind(hyper, "h", function()
+	hs.grid.set(hs.application.open("net.kovidgoyal.kitty"):mainWindow(), screenPositions.left)
+	hs.grid.set(hs.application.open("org.hammerspoon.Hammerspoon"):mainWindow(), screenPositions.right)
+end)
