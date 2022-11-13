e79c842a788eb7b0d33e8dde723989d957e30918
Dom H <dom@hxy.io>
2022-11-13 12:22:23 +0000

    hs: Swap hyper key for a leader key

--- dotfiles/hammerspoon.lua
+++ dotfiles/hammerspoon.lua
@@ -1,7 +1,19 @@
 require "scratchpad"
 
-local hyper = {"control", "option", "command"}
-local shift_hyper = {"shift", "control", "option", "command"}
+-- Set up our leader key (I've remapped Capslock to F19 elsewhere)
+m = hs.hotkey.modal.new('', 'f19')
+
+function m:entered()
+  hs.alert'Entered mode'
+  hs.timer.doAfter(2, function() m:exit() end)
+end
+
+function m:exited()
+  hs.alert'Exited mode'
+end
+
+m:bind('', 'escape', function() m:exit() end)
+m:bind('', 'f19', function() m:exit() end)
 
 -- Set up our grid
 hs.window.animationDuration = 0
@@ -82,78 +94,86 @@ end
 
 -- Show apps with their hotkey
 for k, v in pairs(appKeys) do
-  hs.hotkey.bind(hyper, k, function()
+  m:bind("", k, function()
     hideAppsExcept({v})
     hs.grid.set(hs.application.get(v):mainWindow(), screenPositions.big)
+    m:exit()
   end)
 end
 
 -- Show web apps (via tabs in Google Chrome) with their hotkey
 for k, v in pairs(webAppKeys) do
-  hs.hotkey.bind(hyper, k, function()
+  m:bind("", k, function()
     openChromeTab(v)
     hideAppsExcept({"com.google.Chrome"})
     hs.grid.set(hs.window.focusedWindow(), screenPositions.big)
+    m:exit()
   end)
 end
 
 -- Split screen with another app
 for k, v in pairs(appKeys) do
-  hs.hotkey.bind(shift_hyper, k, function()
+  m:bind("shift", k, function()
     hs.grid.set(hs.window.focusedWindow(), screenPositions.left)
     hs.grid.set(hs.application.open(v):mainWindow(), screenPositions.right)
+    m:exit()
   end)
 end
 
 -- Navigate windows
-hs.hotkey.bind(hyper, "up", function() hs.window.filter.focusNorth() end)
-hs.hotkey.bind(hyper, "right", function() hs.window.filter.focusEast() end)
-hs.hotkey.bind(hyper, "down", function() hs.window.filter.focusSouth() end)
-hs.hotkey.bind(hyper, "left", function() hs.window.filter.focusWest() end)
+m:bind("", "up", function() hs.window.filter.focusNorth() m:exit() end)
+m:bind("", "right", function() hs.window.filter.focusEast() m:exit() end)
+m:bind("", "down", function() hs.window.filter.focusSouth() m:exit() end)
+m:bind("", "left", function() hs.window.filter.focusWest() m:exit() end)
 
 -- Resize windows
-hs.hotkey.bind(shift_hyper, "up", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.big) end)
-hs.hotkey.bind(shift_hyper, "right", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.right) end)
-hs.hotkey.bind(shift_hyper, "down", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.middle) end)
-hs.hotkey.bind(shift_hyper, "left", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.left) end)
+m:bind("shift", "up", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.big) m:exit() end)
+m:bind("shift", "right", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.right) m:exit() end)
+m:bind("shift", "down", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.middle) m:exit() end)
+m:bind("shift", "left", function() hs.grid.set(hs.window.focusedWindow(), screenPositions.left) m:exit() end)
 
 -- Window layout for common terminal/browser work
-hs.hotkey.bind(hyper, "1", function()
+m:bind("", "1", function()
   hideAppsExcept({"com.google.Chrome", "net.kovidgoyal.kitty"})
   hs.grid.set(hs.application.open("com.google.Chrome"):mainWindow(), screenPositions.left)
   hs.grid.set(hs.application.open("net.kovidgoyal.kitty"):mainWindow(), screenPositions.right)
+  m:exit()
 end)
 
 -- Window layout for virtual meetings
-hs.hotkey.bind(hyper, "2", function()
+m:bind("", "2", function()
   hideAppsExcept({"us.zoom.xos", "pro.writer.mac", "com.google.Chrome"})
   hs.grid.set(hs.application.open("us.zoom.xos"):getWindow('Zoom Meeting'), screenPositions.left_top)
   hs.grid.set(hs.application.open("pro.writer.mac"):mainWindow(), screenPositions.left_bottom)
   hs.grid.set(hs.application.open("com.google.Chrome"):mainWindow(), screenPositions.right)
   hs.application.get("us.zoom.xos"):getWindow('Zoom Meeting'):close()
   openChromeTab("app.slack.com")
+  m:exit()
 end)
 
 -- Window layout for planning
-hs.hotkey.bind(hyper, "3", function()
+m:bind("", "3", function()
   hideAppsExcept({"com.apple.iCal", "com.apple.reminders"})
   hs.grid.set(hs.application.open("com.apple.iCal"):mainWindow(), screenPositions.left)
   hs.grid.set(hs.application.open("com.apple.reminders"):mainWindow(), screenPositions.right)
+  m:exit()
 end)
 
 -- Window layout for completing timesheets
-hs.hotkey.bind(hyper, "4", function()
+m:bind("", "4", function()
   hideAppsExcept({"pro.writer.mac", "com.google.Chrome"})
   hs.grid.set(hs.application.open("com.google.Chrome"):mainWindow(), screenPositions.left)
   hs.grid.set(hs.application.open("pro.writer.mac"):mainWindow(), screenPositions.right)
   openChromeTab("harvestapp.com")
+  m:exit()
 end)
 
 -- Window layout for working on Hammerspoon
-hs.hotkey.bind(hyper, "h", function()
+m:bind("", "h", function()
   hideAppsExcept({"net.kovidgoyal.kitty", "org.hammerspoon.Hammerspoon"})
   hs.grid.set(hs.application.open("net.kovidgoyal.kitty"):mainWindow(), screenPositions.left)
   hs.grid.set(hs.application.open("org.hammerspoon.Hammerspoon"):mainWindow(), screenPositions.right)
   hs.console.clearConsole()
   hs.reload()
+  m:exit()
 end)
