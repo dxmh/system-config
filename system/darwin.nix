05b09c42db0125d298eb5e8909577761d25473f2
Dom H <dom@hxy.io>
2022-12-06 15:09:22 +0000

    refactor: Move macOS system config into darwin.nix

---
+++ system/darwin.nix
@@ -0,0 +1,73 @@
+{ pkgs, ... }: {
+
+  imports = [
+    ./brew.nix
+  ];
+
+  services.nix-daemon.enable = true;
+
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 7d";
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      iosevka
+    ];
+  };
+
+  security.pam.enableSudoTouchIdAuth = true;
+
+  system.defaults.dock = {
+    appswitcher-all-displays = true;
+    autohide = false;
+    autohide-delay = 0.0;
+    autohide-time-modifier = 0.2; # Animation duration
+    mineffect = "scale";
+    minimize-to-application = true;
+    mru-spaces = false; # Don't automatically rearrange spaces
+    orientation = "right";
+    show-process-indicators = false; # Show dots for open applications
+    show-recents = false;
+    static-only = false; # Show only open applications in the Dock
+    tilesize = 40;
+  };
+
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    FXPreferredViewStyle = "clmv"; # Column view
+    QuitMenuItem = true;
+    ShowPathbar = true;
+    ShowStatusBar = true;
+  };
+
+  system.defaults.NSGlobalDomain = {
+    NSNavPanelExpandedStateForSaveMode = true; # Use expanded save panel by default
+    KeyRepeat = 2; # Fastest repeat
+    InitialKeyRepeat = 15; # Shortest delay until repeat
+    "com.apple.trackpad.enableSecondaryClick" = true;
+    "com.apple.trackpad.scaling" = 3.0; # Slowest is 0, fastest is 3
+  };
+
+  system.defaults.screencapture = {
+    disable-shadow = true;
+  };
+
+  system.defaults.trackpad = {
+    ActuationStrength = 0; # Silent clicking
+    Clicking = true; # Tap to click
+    TrackpadThreeFingerDrag = true;
+    FirstClickThreshold = 0; # "Light" firmness required
+    SecondClickThreshold = 0; # "Light" firmness required
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    nonUS.remapTilde = true;
+    remapCapsLockToControl = true;
+  };
+
+}
