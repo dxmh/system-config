155d75c3fb1c5a6b160b628be5e5dec13e831af9
Dom H <dom@hxy.io>
2022-12-06 15:38:44 +0000

    refactor: Group options together

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,18 +1,22 @@
 { lib, pkgs, isDarwin, ... }: {
 
-  nix.package = pkgs.nixVersions.stable;
-  nix.gc = {
-    automatic = true;
-    options = "--delete-older-than 7d";
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      warn-dirty = false
+    '';
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+    };
+    package = pkgs.nixVersions.stable;
+    settings.auto-optimise-store = true;
   };
-  nix.settings.auto-optimise-store = true;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-    warn-dirty = false
-  '';
 
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnsupportedSystem = true; # for packages that don't specifically state aarch64 compatibility
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnsupportedSystem = true; # for packages that don't specifically state aarch64 compatibility
+  };
 
   home-manager = {
     useGlobalPkgs = true;
