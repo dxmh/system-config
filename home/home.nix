56e22980671530e82e9a4929f9ddc6fad984cfb6
Dom H <dom@hxy.io>
2022-12-13 17:38:18 +0000

    home: Set Helix as $EDITOR

--- home/home.nix
+++ home/home.nix
@@ -22,6 +22,7 @@
 
   home.sessionVariables = {
     DIRENV_LOG_FORMAT = ""; # https://github.com/direnv/direnv/issues/68
+    EDITOR = "hx";
     FZF_DEFAULT_COMMAND = "rg --files --hidden --glob !.git/";
     FZF_DEFAULT_OPTS = "--color=light";
     MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
