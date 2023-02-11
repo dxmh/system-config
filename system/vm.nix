0ee34f436b023ce547347468b48990b0e7c283d8
Dom H <dom@hxy.io>
2023-02-11 10:19:54 +0000

    refactor: Move VM config out of flake

---
+++ system/vm.nix
@@ -0,0 +1,20 @@
+{ pkgs, ...}: {
+
+  system.stateVersion = "22.05";
+
+  # Configure networking
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+
+  # Create user "test"
+  services.getty.autologinUser = "test";
+  users.users.test.isNormalUser = true;
+
+  # Enable passwordless ‘sudo’ for the "test" user
+  users.users.test.extraGroups = ["wheel"];
+  security.sudo.wheelNeedsPassword = false;
+
+  # Make VM output to the terminal instead of a separate window
+  virtualisation.vmVariant.virtualisation.graphics = false;
+
+}
