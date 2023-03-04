2c7079f5af2c82cc26097fda90a3f9f4aa086a37
Dom H <dom@hxy.io>
2023-03-04 15:22:34 +0000

    kitty: Switch to built-in Catppuccin Mocha theme

--- home/kitty.nix
+++ home/kitty.nix
@@ -12,6 +12,7 @@
     darwinLaunchOptions = [
       "--title='kitty'"
     ];
+    theme = "Catppuccin-Mocha";
     extraConfig = builtins.readFile ./dotfiles/kitty.conf;
   };
 
@@ -25,13 +26,6 @@
     source = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/yurikhan/kitty-smart-scroll/8aaa91/smart_scroll.py";
       sha256 = "sha256:0p6x51gf4m2r61w894cvzhy6cmvzmadz0jbvvj0clcvq1vh9kc63";
-    };
-  };
-
-  home.file.".config/kitty/themes/mocha.conf" = {
-    source = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/kitty/37a9bcac429cdd04c0ecb4041ce6c6e3d4b53f42/mocha.conf";
-      sha256 = "sha256:1mk9y8mcrq4yzlfw9jn8j80mm2djzrihglvp71vl0513fdyf8gb1";
     };
   };
 }
