a7740988e4a8e549a77d7c38a955aed504872f4f
Dom H <dom@hxy.io>
2023-03-05 17:08:56 +0000

    nix: Add users to thebox

--- system/thebox.nix
+++ system/thebox.nix
@@ -29,4 +29,19 @@
       options = ["defaults" "nofail"];
     };
   };
+
+  users = {
+    mutableUsers = false;
+    users.dom = {
+      hashedPassword = "$y$j9T$f3NK6qyFlkDxH.Qk/UzFp.$F5jg4/ZBSD5nOOxH53cowSYh6sNJUwxry5lQy2IzNC3";
+      uid = 1000;
+    };
+    users.poppy = {
+      createHome = false;
+      isNormalUser = true;
+      hashedPassword = "$y$j9T$tmtrE/Wd/5bXWi6A34zZ.0$Tn0m6bzc5iDAMuY/3qHw9IFdX2OCojPAcFSEkQW0sg1";
+      shell = null;
+      uid = 1001;
+    };
+  };
 }
