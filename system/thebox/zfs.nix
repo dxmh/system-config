18e52962ef7f0f42e05ca23b142ee45e6675d0fd
Dom H <dom@hxy.io>
2023-03-10 11:31:36 +0000

    nix: Automatically unlock LUKS disk at boot

--- system/thebox/zfs.nix
+++ system/thebox/zfs.nix
@@ -1,6 +1,14 @@
 {...}: {
   boot.supportedFilesystems = ["zfs"];
 
+  # Automatically decrypt LUKS volumes on boot so ZFS can use them
+  environment.etc.crypttab = {
+    mode = "0600";
+    text = ''
+      luks_test UUID=5f53fec8-88ca-473e-86f1-cac849b33336 /etc/luks_keyfile.bin
+    '';
+  };
+
   networking.hostId = "000a020a";
 
   services.zfs.autoScrub = {
