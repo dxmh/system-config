034830223c2186f315180fa2c07cc5ddcde70e18
Dom H <dom@hxy.io>
2023-03-10 08:48:43 +0000

    nix: Don't patch kitty to use macOS Terminal icon
    This reverts commit 7069085dc8d5b2a691b0b98603d2d5bb30fdc63a.
    
    It doesn't work on Linux, I'll need to reconfigure it to apply only to
    Darwin hosts.

--- flake.nix
+++ flake.nix
@@ -21,12 +21,6 @@
     overlays = [
       (final: prev: {
         helix = helix.packages.${prev.system}.default;
-        kitty = prev.kitty.overrideAttrs (oldAttrs: {
-          postInstall = ''
-            cp -v /System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns \
-              $out/Applications/kitty.app/Contents/Resources/kitty.icns
-          '';
-        });
       })
     ];
   in {
