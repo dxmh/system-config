3c9d458b0d6b9c0b5105af5ea0d54d5199fea967
Dom H <dom@hxy.io>
2023-03-13 17:10:07 +0000

    nix: Use home-manager release branch
    Rather than unstable.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     helix.url = "github:helix-editor/helix";
     helix.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
