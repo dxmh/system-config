d7a8442d88ef3778d4a7db153ded30a618ef90df
Dom H <dom@hxy.io>
2023-03-20 09:18:42 +0000

    nix: Enable `timesyncd`

--- system/cbd/configuration.nix
+++ system/cbd/configuration.nix
@@ -30,6 +30,8 @@
 
   services.tailscale.enable = true;
 
+  services.timesyncd.enable = true;
+
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
   sops.defaultSopsFile = ./secrets.yaml;
 
