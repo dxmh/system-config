eabcb98cd185690fdbf5a1a46e71b8cce460769c
Dom H <dom@hxy.io>
2023-03-23 17:40:41 +0000

    refactor: Consolidate Helix language server config
    Now they don't appear in my path, and they're configured in only one place.

--- home/helix.nix
+++ home/helix.nix
@@ -63,7 +63,7 @@
         # Use ltex-ls an an alternative LSP for Markdown, to provide spelling suggestions
         # https://github.com/helix-editor/helix/wiki/How-to-install-the-default-language-servers#markdown
         name = "markdown";
-        language-server = {command = "ltex-ls";};
+        language-server.command = "${pkgs.ltex-ls}/bin/ltex-ls";
         file-types = ["md"];
         scope = "source.markdown";
         roots = [];
@@ -71,29 +71,36 @@
       }
       {
         name = "json";
-        language-server = {
-          command = "vscode-json-languageserver";
-          args = ["--stdio"];
-        };
+        auto-format = true;
+        language-server.command = "${pkgs.nodePackages.vscode-json-languageserver}/bin/vscode-json-languageserver";
+        language-server.args = ["--stdio"];
       }
       {
         name = "nix";
-        formatter = {
-          command = "alejandra";
-          args = ["-"];
-        };
         auto-format = true;
+        formatter.command = "${pkgs.alejandra}/bin/alejandra";
+        formatter.args = ["-"];
+        language-server.command = "${pkgs.nil}/bin/nil";
+      }
+      {
+        name = "bash";
+        language-server.command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+        language-server.args = ["start"];
+      }
+      {
+        name = "hcl";
+        auto-format = true;
+        language-server.command = "${pkgs.terraform-ls}/bin/terraform-ls";
+        language-server.args = ["serve"];
+        language-server.language-id = "terraform";
+      }
+      {
+        name = "tfvars";
+        auto-format = true;
+        language-server.command = "${pkgs.terraform-ls}/bin/terraform-ls";
+        language-server.args = ["serve"];
+        language-server.language-id = "terraform-vars";
       }
     ];
   };
-
-  # Language servers
-  home.packages = with pkgs; [
-    alejandra
-    nodePackages.bash-language-server
-    nodePackages.vscode-json-languageserver
-    nil
-    ltex-ls
-    terraform-ls
-  ];
 }
