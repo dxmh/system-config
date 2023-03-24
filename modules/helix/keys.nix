a5ca49e619c678dc15a9fc9a35e66fcafe1a2a39
Dom H <dom@hxy.io>
2023-03-24 21:09:57 +0000

    refactor: Flatten attribute set
    To make it easier to tell which mode the key mappings apply to.

--- modules/helix/keys.nix
+++ modules/helix/keys.nix
@@ -1,39 +1,35 @@
 {
-  normal = {
-    "#" = "toggle_comments";
-    "C-C" = "yank_main_selection_to_clipboard";
-    "C-c" = "yank_joined_to_clipboard";
-    "C-l" = ":reload";
-    "C-v" = "replace_selections_with_clipboard";
+  normal."#" = "toggle_comments";
+  normal."C-C" = "yank_main_selection_to_clipboard";
+  normal."C-c" = "yank_joined_to_clipboard";
+  normal."C-l" = ":reload";
+  normal."C-v" = "replace_selections_with_clipboard";
 
-    space.c.r = ":reset-diff-change";
-    space.c.p = "goto_prev_change";
-    space.c.n = "goto_next_change";
+  normal.space.c.r = ":reset-diff-change";
+  normal.space.c.p = "goto_prev_change";
+  normal.space.c.n = "goto_next_change";
 
-    space.e = ":reflow";
-    space.n = ":new";
-    space.space = "goto_last_accessed_file";
+  normal.space.e = ":reflow";
+  normal.space.n = ":new";
+  normal.space.space = "goto_last_accessed_file";
 
-    space.q = ":q";
-    space.Q = ":q!";
-    space.w = ":w";
-    space.W = ":w!";
-    space.x = ":x";
+  normal.space.q = ":q";
+  normal.space.Q = ":q!";
+  normal.space.w = ":w";
+  normal.space.W = ":w!";
+  normal.space.x = ":x";
 
-    space.o.w = ":toggle-option soft-wrap.enable";
-    space.o.c = ":toggle-option cursorline";
+  normal.space.o.w = ":toggle-option soft-wrap.enable";
+  normal.space.o.c = ":toggle-option cursorline";
 
-    Z.d = "half_page_down";
-    Z.u = "half_page_up";
+  normal.Z.d = "half_page_down";
+  normal.Z.u = "half_page_up";
 
-    # Horrible bodge for toggling spellcheck:
-    "[".o.s = ":lang markdown";
-    "]".o.s = [":config-reload" ":lsp-restart"];
-  };
+  # Horrible bodge for toggling spellcheck:
+  normal."[".o.s = ":lang markdown";
+  normal."]".o.s = [":config-reload" ":lsp-restart"];
 
-  select = {
-    "#" = "toggle_comments";
-    "C-C" = "yank_main_selection_to_clipboard";
-    "C-c" = "yank_joined_to_clipboard";
-  };
+  select."#" = "toggle_comments";
+  select."C-C" = "yank_main_selection_to_clipboard";
+  select."C-c" = "yank_joined_to_clipboard";
 }
