98b28911b1556ba47af3370c088ef9a1918c7d77
Dom H <dom@hxy.io>
2023-03-25 14:53:32 +0000

    git: Configure default username and email address

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,13 +4,27 @@
   mainUser,
   ...
 }: {
-  options.hxy.git.enable = lib.mkEnableOption "Enable git configuration";
+  options.hxy.git = {
+    enable = lib.mkEnableOption "Enable git configuration";
+    userName = lib.mkOption {
+      type = lib.types.str;
+      default = "Dom H";
+      description = "Default user name to use.";
+    };
+    userEmail = lib.mkOption {
+      type = lib.types.str;
+      default = "dom@hxy.io";
+      description = "Default user email to use.";
+    };
+  };
 
   config = lib.mkIf config.hxy.git.enable {
     home-manager.users.${mainUser}.programs.git = {
       enable = true;
       extraConfig = {
         push.autoSetupRemote = true;
+        user.name = config.hxy.git.userName;
+        user.email = config.hxy.git.userEmail;
       };
       delta = {
         enable = true;
--- system/setze/configuration.nix
+++ system/setze/configuration.nix
@@ -15,13 +15,6 @@
 
   security.sudo.wheelNeedsPassword = false;
 
-  home-manager.users.${mainUser} = {
-    programs.git = {
-      userEmail = "dom@hxy.io";
-      userName = "Dom H";
-    };
-  };
-
   users.users.${mainUser}.extraGroups = ["docker"];
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = false; # Start on-demand by socket activation rather than boot
