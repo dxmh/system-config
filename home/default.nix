69b21df7a5088eb5ac2d6d24990ee7cf1e3b4556
Dom H <dom@hxy.io>
2023-03-25 14:02:03 +0000

    nix: Move starship config into fish module

--- home/default.nix
+++ home/default.nix
@@ -27,35 +27,4 @@
       ".direnv"
     ];
   };
-
-  # http://starship.rs/config/
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      character.error_symbol = "[\⟩](red)";
-      character.success_symbol = "[\⟩](dimmed white)";
-      docker_context.disabled = true;
-      format = "\$all\$hostname\$status\$character";
-      git_status.disabled = true;
-      git_branch.symbol = " ";
-      hostname = {
-        ssh_only = false;
-        ssh_symbol = "";
-        format = "[\$ssh_symbol\$hostname](\$style) ";
-        style = "dimmed white";
-      };
-      line_break.disabled = false;
-      nodejs.disabled = true;
-      package.disabled = true;
-      php.disabled = true;
-      python.symbol = " ";
-      status.disabled = false;
-      status.symbol = "⨯";
-      time.disabled = true;
-      username.disabled = true;
-      vagrant.disabled = true;
-    };
-  };
 }
