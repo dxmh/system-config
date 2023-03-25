9acf3fc33fb2f7ed434e554d4d9b078b97d07fed
Dom H <dom@hxy.io>
2023-03-25 08:41:07 +0000

    nix: Use `nix-community` binary cache

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -19,10 +19,12 @@
       substituters = [
         "https://dxmh.cachix.org/"
         "https://helix.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "dxmh.cachix.org-1:mxvbwbVG4qos714NloY6lzrM1lTEO1lHVcJVF1Ngyic="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
