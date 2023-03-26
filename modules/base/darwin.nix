df5b4e371d41430637a4d487f854323ee868ec2a
Dom H <dom@hxy.io>
2023-03-26 08:48:28 +0100

    nix: Show macOS dock at the bottom

--- modules/base/darwin.nix
+++ modules/base/darwin.nix
@@ -44,15 +44,15 @@
 
     system.defaults.dock = {
       appswitcher-all-displays = true;
-      autohide = true;
+      autohide = false;
       autohide-delay = 0.0;
       autohide-time-modifier = 0.2; # Animation duration
       mineffect = "scale";
       minimize-to-application = true;
       mru-spaces = false; # Don't automatically rearrange spaces
-      orientation = "left";
+      orientation = "bottom";
       show-process-indicators = false; # Show dots for open applications
-      show-recents = true;
+      show-recents = false;
       static-only = false; # Whether to show only open applications in the Dock
       tilesize = 40;
       # Trigger mission control via hot corners:
