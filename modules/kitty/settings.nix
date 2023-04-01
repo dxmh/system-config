9aa77b4dad2b4d866cc8dab88b1d5039d98a3008
Dom H <dom@hxy.io>
2023-04-01 07:57:32 +0100

    kitty: More macOS-native feeling

--- modules/kitty/settings.nix
+++ modules/kitty/settings.nix
@@ -17,7 +17,7 @@
   macos_option_as_alt = true;
   macos_quit_when_last_window_closed = false;
   macos_show_window_title_in = "window";
-  macos_titlebar_color = "#010409";
+  macos_titlebar_color = "dark";
   mouse_map = "right press ungrabbed mouse_select_command_output";
   placement_strategy = "top-left";
   remote_kitty = "no";
@@ -31,7 +31,7 @@
   tab_bar_align = "left";
   tab_bar_edge = "bottom";
   tab_bar_margin_color = "#010409";
-  tab_bar_margin_height = "3 3";
+  tab_bar_margin_height = "6 4";
   tab_bar_margin_width = 14;
   tab_bar_min_tabs = 1;
   tab_bar_style = "separator";
