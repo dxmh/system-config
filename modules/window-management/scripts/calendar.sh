d5df2f03f0644ff110ca11b7b03f934eb8079482
Dom H <dom@hxy.io>
2023-08-19 17:24:06 +0100

    refactor: Less obscure icalBuddy command

--- modules/window-management/scripts/calendar.sh
+++ modules/window-management/scripts/calendar.sh
@@ -6,7 +6,20 @@ color=0xcc000000
 font=medium
 
 # Get the next event in the format "12:34 My Event"
-next_event=$(icalBuddy -ea -b "" -npn -nc -iep "datetime,title" -ps "| |" -po "datetime,title" -tf "%H:%M" -eed -li 1 -n eventsToday)
+next_event=$(
+  icalBuddy \
+    --bullet "" \
+    --excludeAllDayEvents \
+    --excludeEndDates \
+    --includeOnlyEventsFromNowOn \
+    --limitItems 1 \
+    --noCalendarNames \
+    --noPropNames \
+    --propertyOrder "datetime,title" \
+    --propertySeparators "| |" \
+    --timeFormat "%H:%M" \
+    eventsToday
+)
 
 if [ ! "$next_event" ]; then
   result="No upcoming events"
