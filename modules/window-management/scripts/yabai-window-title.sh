a1ddb182e109939412bd23669565df0d95025867
Dom H <dom@hxy.io>
2023-08-23 17:51:01 +0100

    darwin: Show yabai window stacks in menubar

--- modules/window-management/scripts/yabai-window-title.sh
+++ modules/window-management/scripts/yabai-window-title.sh
@@ -3,4 +3,11 @@
 # Get the title of the currently focused window
 title=$(yabai -m query --windows --window | jq -r .title)
 
-sketchybar --set yabai_window_title label="${title}"
+stack_index=$(yabai -m query --windows --window | jq -r '.["stack-index"]')
+
+if [ "$stack_index" -gt 0 ]; then
+  stack_total=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
+  stack_status="􀯰 Window stack ${stack_index}/${stack_total}"
+fi
+
+sketchybar --set yabai_window_title label="${title}   ${stack_status}"
