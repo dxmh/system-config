11063be3168d5e5b3d8d4821c4d16f74ac33e5ba
Dom H <dom@hxy.io>
2024-01-09 16:42:19 +0000

    thebox: Update sendmail email address

--- hosts/thebox/sendmail.nix
+++ hosts/thebox/sendmail.nix
@@ -10,7 +10,7 @@
   # Configure mappings between a local address and a list of replacement
   # addresses. In this case we just configure a single "catch all" address.
   environment.etc.aliases.text = ''
-    default: dom@hxy.io
+    default: dom@domhay.com
   '';
 
   # Overwrite NixOS's msmtprc config file with the one from our secrets
