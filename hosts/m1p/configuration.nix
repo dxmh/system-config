bd0091b45498918d557add6ad897986a825fa662
Dom H <dom@domhay.com>
2024-11-03 11:34:54 +0000

    m1p: Enable NetworkManager to utilise iwd backend

--- hosts/m1p/configuration.nix
+++ hosts/m1p/configuration.nix
@@ -59,6 +59,8 @@
     desktopManager.gnome.enable = true;
   };
 
+  networking.networkmanager.wifi.backend = "iwd";
+
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
